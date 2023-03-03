b3baf0b704e44725fcfec48b591f6ae1a4cffb87
Elliot Thomas <elliot@eth0net.io>
2023-03-03 08:36:10 +0000

    Fix animation and add background

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,8 @@ const MAX_SPEED = 100
 const FRICTION = 400
 
 @onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -12,12 +14,13 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	
-	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		animationPlayer.play("RunRight")
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		animationPlayer.play("IdleRight")
 
 	move_and_slide()
