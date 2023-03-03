85f70ac0277c689c8ce347431d832f455df4e4d0
Elliot Thomas <elliot@eth0net.io>
2023-03-03 16:43:33 +0000

    Add attack animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,9 @@ const FRICTION = 400
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
